124ad334ab4a92a485ddcef7f48882f65e00c551
ignchap27 <i.chaparro@uniandes.edu.co>
2025-03-31 16:45:13 -0500

    Cuadrado de color

--- src/engine/game_engine.py
+++ src/engine/game_engine.py
@@ -1,6 +1,13 @@
+import pygame
+
 class GameEngine:
     def __init__(self) -> None:
+        pygame.init()
+        self.screen = pygame.display.set_mode((640, 360), pygame.SCALED)
+        self.clock = pygame.time.Clock()
         self.is_running = False
+        self.frame_rate = 60
+        self.delta_time = 0
 
     def run(self) -> None:
         self._create()
@@ -19,13 +26,16 @@ class GameEngine:
         pass
 
     def _process_events(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.is_running = False
 
     def _update(self):
         pass
 
     def _draw(self):
-        pass
+        self.screen.fill((0, 100, 200))
+        pygame.display.flip()
 
     def _clean(self):
-        pass
+        pygame.quit()
